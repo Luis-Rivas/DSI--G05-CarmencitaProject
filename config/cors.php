774d9be729019c1fb6d3371b3c326511fd841f0e
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2023-06-17 15:02:51 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://daiv05.github.io', 'https://main--spectacular-granita-d61b49.netlify.app','*'],
+    'allowed_origins' => ['https://daiv05.github.io', 'https://main--spectacular-granita-d61b49.netlify.app','https://81cc-168-90-108-17.ngrok-free.app/'],
 
     'allowed_origins_patterns' => [],
 
